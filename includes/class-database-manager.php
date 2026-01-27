2d69152b9309ed0451c1e20923719379b8b6d029
Mike Miler <mike@miler.de>
2026-01-27 21:36:42 +0100

    Fix error message when submitting form

--- includes/class-database-manager.php
+++ includes/class-database-manager.php
@@ -405,28 +405,21 @@ class Seculoco_Database_Manager {
 		$admin_url = admin_url( 'admin.php?page=secure-login-collector' );
 
 		// translators: %s is the site name.
-		$subject = sprintf( __( '[%s] New Secure Login Data Received', 'secure-login-collector' ), $site_name );
+		$subject = '[' . $site_name . '] ' . __( 'New Secure Login Data Received', 'secure-login-collector' );
 
-		// translators: %1$s is sender email, %2$s is sender name, %3$s is submission time, %4$s is admin URL, %5$s is site name, %6$s is site URL.
-		$message_template = __(
-			"Hello,\n\nNew secure login data has been submitted to your website.\n\nSender Email: %1\$s\nSender Name: %2\$s\nSubmitted: %3\$s\n\nTo view and decrypt the login data, please visit:\n%4\$s\n\nThis is an automated notification from %5\$s\nWebsite: %6\$s",
-			'secure-login-collector'
-		);
+		$display_sender_name = ! empty( $sender_name ) ? $sender_name : __( 'Not provided', 'secure-login-collector' );
+		$submission_time     = current_time( 'Y-m-d H:i:s' );
 
-		// Ensure line breaks render correctly in plain-text email clients.
-		$message = str_replace(
-			"\n",
-			"\r\n",
-			sprintf(
-				$message_template,
-				$sender_email,
-				! empty( $sender_name ) ? $sender_name : __( 'Not provided', 'secure-login-collector' ),
-				current_time( 'Y-m-d H:i:s' ),
-				$admin_url,
-				$site_name,
-				$site_url
-			)
-		);
+		// Build message with simple concatenation - no sprintf() risks.
+		$message = __( 'Hello,', 'secure-login-collector' ) . "\r\n\r\n";
+		$message .= __( 'New secure login data has been submitted to your website.', 'secure-login-collector' ) . "\r\n\r\n";
+		$message .= __( 'Sender Email:', 'secure-login-collector' ) . ' ' . $sender_email . "\r\n";
+		$message .= __( 'Sender Name:', 'secure-login-collector' ) . ' ' . $display_sender_name . "\r\n";
+		$message .= __( 'Submitted:', 'secure-login-collector' ) . ' ' . $submission_time . "\r\n\r\n";
+		$message .= __( 'To view and decrypt the login data, please visit:', 'secure-login-collector' ) . "\r\n";
+		$message .= $admin_url . "\r\n\r\n";
+		$message .= __( 'This is an automated notification from', 'secure-login-collector' ) . ' ' . $site_name . "\r\n";
+		$message .= __( 'Website:', 'secure-login-collector' ) . ' ' . $site_url;
 
 		$parsed_url = wp_parse_url( $site_url );
 		$host       = isset( $parsed_url['host'] ) ? $parsed_url['host'] : 'localhost';
